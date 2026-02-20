bc8c031e77dbd4e9158dafc490ebaf18e4e5eb04
yoni <unique.center.yoni@gmail.com>
2026-02-20 22:36:41 +0200

    fix: add base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // הגדרת ה-base הכרחית כדי שהאתר לא יהיה לבן בגיטהאב
+      base: '/smartkis/', 
       server: {
         port: 3000,
-        host: '0.0.0.0',
+        host: '0.0.0.0', // מאפשר גישה למכשירים אחרים ברשת (Network)
+        allowedHosts: true,
       },
       plugins: [react()],
       define: {
