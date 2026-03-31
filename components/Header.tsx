cd232d7f699a62aa105b006fb9f0509317de7124
alon46678-boop <alon46678@gmail.com>
2026-03-31 17:11:14 +0300

    sevev46

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,14 +8,14 @@ const Header: React.FC<HeaderProps> = ({ onLogoClick }) => {
   return (
     <header className="flex flex-col items-center justify-center text-center px-2">
       <div
-        className="relative inline-block select-none group rounded-2xl bg-white px-4 py-2 shadow-sm"
+        className="relative inline-block select-none"
         onClick={onLogoClick}
         style={{ cursor: onLogoClick ? 'pointer' : 'default' }}
       >
         <img
           src={`${import.meta.env.BASE_URL}logo2.svg`}
           alt="חכם בכיס"
-          className="h-28 sm:h-40 lg:h-48 w-auto drop-shadow-lg group-hover:drop-shadow-xl transition-all duration-300"
+          className="h-28 sm:h-40 lg:h-48 w-auto drop-shadow-lg transition-all duration-300"
         />
       </div>
     </header>
